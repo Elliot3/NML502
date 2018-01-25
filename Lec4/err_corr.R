000ed21f77b50b09d71646b60e2b40b9fe105037
Elliot Smith <elliot@elliots-imac.attlocal.net>
2018-01-25 17:10:05 -0600

    fixes

--- Lec4/err_corr.R
+++ Lec4/err_corr.R
@@ -1,7 +1,7 @@
 x <- matrix(c(1,0,0,0,1,0,0,0,0,0,0,1), nrow = 4, ncol = 3, byrow = T)
 y <- matrix(c(2,1,-2,-2,1,4,3,-2,2), nrow = 3, ncol = 3, byrow = T)
-mu <- 0.1
-n <- 100
+mu <- 0.2
+n <- 1000
 tol <- 0.0001
 
 # err_corr(x, y, mu, n, tol)
@@ -21,21 +21,20 @@ err_corr <- function(x, y, mu, n, tol) {
         
         for (k in 1:x_dims[2]) {
             
-            mem_matrix <- mem_matrix + mu * (y[,k] - mem_matrix * x[,k]) * x[,k]
+            mem_matrix <- mem_matrix + mu * (y[,k] - mem_matrix %*% x[,k]) %*% x[,k]
             
         }
         
-        if (max(svd(y - mem_matrix %*% x)$d) <= tol) {
+        if (norm((y - mem_matrix %*% x), "2") <= tol) {
             
             print("Gradient Search Terminated ===>>> ||y - mem_matrix*x|| <= tol")
-            print("Number of Iterations = ")
-            n*i
+            print(paste0("Number of Iterations = ", n * k))
             break
             
         }
         
     }
     
-    mem_matrix
+    return(mem_matrix)
     
 }
