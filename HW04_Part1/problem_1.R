53e3399264ecacfd58ced9624604e8cbf4978ec7
Elliot Smith <esmith2642@gmail.com>
2018-02-06 23:55:28 -0600

    some work

--- HW04_Part1/problem_1.R
+++ HW04_Part1/problem_1.R
@@ -14,3 +14,18 @@ der_hyp_tan <- function(x) {
     
 }
 
+## Initialize the network
+
+num_layers <- 2
+num_neurons <- c(2, 3, 1)
+weights <- list()
+biases <- list()
+
+for (i in 1:num_layers) {
+    
+    weights[[i]] <- matrix(runif(num_neurons[i] * num_neurons[i + 1]), nrow = num_neurons[i + 1], ncol = num_neurons[i])
+    biases[[i]] <- matrix(runif(num_neurons[i] * num_neurons[i + 1]), nrow = num_neurons[i + 1], ncol = 1)
+    
+}
+
+## Initialize the training parameters
