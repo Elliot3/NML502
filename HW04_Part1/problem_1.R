f8250ed33827c4b0bcde691ae6af938f00188bbb
Elliot Smith <esmith2642@gmail.com>
2018-02-07 00:46:35 -0600

    anoth comm

--- HW04_Part1/problem_1.R
+++ HW04_Part1/problem_1.R
@@ -17,15 +17,25 @@ der_hyp_tan <- function(x) {
 ## Initialize the network
 
 num_layers <- 2
-num_neurons <- c(2, 3, 1)
+num_outputs <- c(2, 3, 1)
+
 weights <- list()
 biases <- list()
 
 for (i in 1:num_layers) {
     
-    weights[[i]] <- matrix(runif(num_neurons[i] * num_neurons[i + 1]), nrow = num_neurons[i + 1], ncol = num_neurons[i])
-    biases[[i]] <- matrix(runif(num_neurons[i] * num_neurons[i + 1]), nrow = num_neurons[i + 1], ncol = 1)
+    weights[[i]] <- matrix(runif(num_outputs[i] * num_outputs[i + 1]), nrow = num_outputs[i + 1], ncol = num_outputs[i])
+    biases[[i]] <- matrix(runif(num_outputs[i] * num_outputs[i + 1]), nrow = num_outputs[i + 1], ncol = 1)
     
 }
 
 ## Initialize the training parameters
+
+n <- 1000
+ler_rate <- 0.001
+
+## Import the training data
+
+x <- matrix(c(1,1,0,0,1,0,1,0), nrow = 2, ncol = 4, byrow = T)
+y <- matrix(c(0,1,1,0), nrow = 4, ncol = 1)
+bias <- matrix(rep(1, 4), nrow = 4, ncol = 1)
