82c02a2ea6da4c6a35890b855a199530ac7180a6
Elliot Smith <esmith2642@gmail.com>
2018-03-06 22:59:48 -0600

    hw6 prob1

--- HW06/HW06.R
+++ HW06/HW06.R
@@ -2,7 +2,17 @@
 
 train_data <- read.table("~/Documents/Rice_University/Spring_2018/NML502/HW04_Part2/iris-train.txt", skip = 8)
 train_attr <- train_data[c(TRUE, FALSE), ]
+train_attr$V1 <- as.numeric(as.character(train_attr$V1))
 train_cats <- train_data[c(FALSE, TRUE), ][ ,2:4]
 
 data <- cbind(train_attr, train_cats)
 names(data) <- c("Sep_L", "Sep_W", "Pet_L", "Pet_W", "Setosa", "Versacolor", "Virginica")
+
+scaled_data <- apply(data[, 1:4], 2, function(x) {(x - mean(x)) / sd(x)})
+
+iris_pca <- prcomp(scaled_data)
+
+eigen_vec <- iris_pca$sdev
+eigen_vals <- iris_pca$rotation
+
+round(iris_pca$rotation %*% t(iris_pca$rotation))
