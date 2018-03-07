6bd2476b364c1df6ee7f9e8bc4c228e47d18714b
Elliot Smith <esmith2642@gmail.com>
2018-03-07 01:16:28 -0600

    small chngs

--- HW06/HW06.R
+++ HW06/HW06.R
@@ -1,3 +1,5 @@
+##### Problem 2.1
+
 ## Load in the iris training data
 
 train_data <- read.table("~/Documents/Rice_University/Spring_2018/NML502/HW04_Part2/iris-train.txt", skip = 8)
@@ -8,11 +10,20 @@ train_cats <- train_data[c(FALSE, TRUE), ][ ,2:4]
 data <- cbind(train_attr, train_cats)
 names(data) <- c("Sep_L", "Sep_W", "Pet_L", "Pet_W", "Setosa", "Versacolor", "Virginica")
 
+## Scale the data
+
 scaled_data <- apply(data[, 1:4], 2, function(x) {(x - mean(x)) / sd(x)})
+
+## Perform the PCA
 
 iris_pca <- prcomp(scaled_data)
 
 eigen_vec <- iris_pca$sdev
 eigen_vals <- iris_pca$rotation
 
+## Confirm we get the identity matrix
+
 round(iris_pca$rotation %*% t(iris_pca$rotation))
+
+##### Problem 2.2
+
