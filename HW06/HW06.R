479eccc719c04210b75325d13fd07f4a096c22c3
Elliot Smith <esmith2642@gmail.com>
2018-03-07 22:30:49 -0600

    still working

--- HW06/HW06.R
+++ HW06/HW06.R
@@ -25,9 +25,11 @@ eigen_vecs <- iris_pca$rotation
 
 iris_pca$rotation %*% t(iris_pca$rotation)
 
+
+
 ##### Problem 2.2
 
-n <- 500
+n <- 50000
 num_outputs <- c(4, 4)
 num_layers <- 1
 ler_rate <- 0.0001
@@ -40,20 +42,55 @@ weights <- matrix(runif(num_outputs[num_layers] * num_outputs[num_layers + 1]),
 
 y <- scaled_data
 
-temp_cell <- integer()
-
 for (i in 1:n) {
     
     rand_ind <- sample(1:75, 1)
     
     x_output <- t(weights) %*% y[rand_ind, ]
     
+    temp_mat <- matrix(0, nrow = 4, ncol = 4)
+    
     for (j in 1:length(y[rand_ind, ])) {
+        
+        if (j == 1) {
             
-            temp_cell[j + 1,] <- temp_cell[j,] + y[rand_ind, ][[j]] * t(weights[j,])
+            temp_mat[j, ] <- c(y[rand_ind, ][j] * (y[rand_ind, ][j] * weights[[j]]),
+                               0, 0, 0)
+            
+        } else if (j == 2) {
+            
+            temp_mat[j, ] <- c(y[rand_ind, ][j] * y[rand_ind, ][j - 1] * weights[[j - 1]],
+                               y[rand_ind, ][j] * y[rand_ind, ][j] * weights[[j]],
+                               0, 0)
+            
+        } else if (j == 3) {
+            
+            temp_mat[j, ] <- c(y[rand_ind, ][j] * y[rand_ind, ][j - 2] * weights[[j - 2]],
+                             y[rand_ind, ][j] * y[rand_ind, ][j - 1] * weights[[j - 1]],
+                             y[rand_ind, ][j] * y[rand_ind, ][j] * weights[[j]],
+                             0)
+            
+        } else if (j == 4) {
+            
+            temp_mat[j, ] <-  c(y[rand_ind, ][j] * y[rand_ind, ][j - 3] * weights[[j - 3]],
+                              y[rand_ind, ][j] * y[rand_ind, ][j - 2] * weights[[j - 2]],
+                              y[rand_ind, ][j] * y[rand_ind, ][j - 1] * weights[[j - 1]],
+                              y[rand_ind, ][j] * y[rand_ind, ][j] * weights[[j]])
+            
+        }
         
     }
     
-    weights <- weights + (ler_rate * y[rand_ind, ] %*% x_output) - (ler_rate * (y[rand_ind, ] %*% temp_cell))
-    
+    weights <- weights +
+        ler_rate * matrix(y[rand_ind, ], ncol = 1) %*% matrix(x_output, nrow = 1) -
+        ler_rate * temp_mat
+
 }
+
+
+
+
+
+
+
+
