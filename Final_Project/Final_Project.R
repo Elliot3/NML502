c42337514b1d60ec2333d033c6b7b0a2a4344b7c
Elliot Smith <esmith2642@gmail.com>
2018-04-14 17:32:11 -0500

    finished the draft of the learning process

--- Final_Project/Final_Project.R
+++ Final_Project/Final_Project.R
@@ -16,6 +16,17 @@ library(dplyr)
 
 
 
+## Function to calculate the Manhattan distance
+
+manhattan_dist <- function(rating1, rating2) {
+    
+    distance <- abs(rating1 - rating2)
+    distance <- sum(distance)
+    
+    return(distance)
+    
+}
+
 ## Function to build the SOM lattice matrix
 
 build_SOM <- function(input_size, matrix_dim) {
@@ -37,7 +48,23 @@ build_SOM <- function(input_size, matrix_dim) {
 
 ## Function to perform the SOM leaarning
 
-learn_SOM <- function(input_data, num_iter, ler_rate, radius) {
+learn_SOM <- function(input_data, SOM_lattice, num_iter, ler_rate, radius, matrix_dim) {
+    
+    ## Container for the current prototype lattice
+    
+    SOM_container <- list()
+    
+    ## Container for the indices
+    
+    SOM_indices <- list()
+    
+    ## Build the list of weight indices
+    
+    SOM_indices[[1]] <- matrix(rep(1:matrix_dim, times = matrix_dim),
+                               nrow = matrix_dim, ncol = matrix_dim)
+    
+    SOM_indices[[2]] <- matrix(rep(1:matrix_dim, times = matrix_dim),
+                               byrow = T, nrow = matrix_dim, ncol = matrix_dim)
     
     ## Record the original ler_rate and radius
     
@@ -63,9 +90,54 @@ learn_SOM <- function(input_data, num_iter, ler_rate, radius) {
         rand_ind <- sample(x = 1:dim(input_data)[1], size = 1)
         input <- matrix(input_data[rand_ind, ], ncol = 1)
         
-        ##### START HERE #####
+        ## Calculate the Euclidean distances
+        
+        list_diffs <- list()
+        vec_dists <- numeric()
+        
+        for (j in 1:dim(SOM_lattice)[2]) {
+            
+            list_diffs[[j]] <- input - SOM_lattice[[j]]
+            vec_dists[j] <- norm(list_diffs[[j]], type = "2")
+            
+        }
+        
+        ## Find the winning PE
+        
+        min_neuron <- min(vec_dists)
+        min_list <- as.vector(which(vec_dists == min(vec_dists), arr.ind = TRUE))
+        
+        x_pt <- floor(min_list / matrix_dim) + 1
+        y_pt <- min_list - ((x_pt - 1) * matrix_dim)
+        
+        min_loc <- c(x_pt, y_pt)
+        
+        ## Calculate the Manhattan distance
+        
+        mat_diffs_y <- abs(SOM_indices[[1]] - min_loc[1])
+        mat_diffs_x <- abs(SOM_indices[[2]] - min_loc[2])
+        man_dist <- mat_diffs_x + mat_diffs_y
+        neighbor_func <- as.vector(exp(-((man_dist)/(radius))^2))
+        
+        ## Update the weights
+        
+        for (j in 1:dim(SOM_lattice)[2]) {
+            
+            SOM_lattice[, j] <- as.matrix(SOM_lattice[, j], ncol = 1) + ler_rate * (neighbor_func[j] * list_diffs[[j]])
+            
+        }
+        
+        ## Add lattice to the container
+        
+        if ((i == 1) || (i %in% seq(from = 0, to = num_iter, length.out = 6))) {
+            
+            SOM_container[[length(SOM_container) + 1]] <- list(i, SOM_lattice)
+            
+        }
         
     }
+    
+    return(SOM_container)
     
 }
 
@@ -120,13 +192,19 @@ SOM_lattice <- build_SOM(input_size, matrix_dim)
 
 
 
+
+
 ########## Perform Analysis ##########
 
 
 
 
 
+##### Learn the Network #####
 
+
+
+learn_results <- learn_SOM(input_data = input_space, SOM_lattice, num_iter, ler_rate, radius, matrix_dim)
 
 
 
