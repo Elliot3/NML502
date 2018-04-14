323b37492348cd3d98f55e7411df37e3ea91a5a8
Elliot Smith <esmith2642@gmail.com>
2018-04-14 15:45:26 -0500

    working on adding the learn function

--- Final_Project/Final_Project.R
+++ Final_Project/Final_Project.R
@@ -35,6 +35,40 @@ build_SOM <- function(input_size, matrix_dim) {
     
 }
 
+## Function to perform the SOM leaarning
+
+learn_SOM <- function(input_data, num_iter, ler_rate, radius) {
+    
+    ## Record the original ler_rate and radius
+    
+    init_ler_rate <- ler_rate
+    init_radius <- radius
+    
+    ## Establish a decay constant
+    
+    decay_constant <- num_iter / log(init_radius)
+    
+    for (i in 1:num_iter) {
+        
+        ## Decay the learning rate
+        
+        ler_rate <- init_ler_rate * exp(-i / num_iter)
+        
+        ## Decay the radius
+        
+        radius <- init_radius * exp(-i / decay_constant)
+        
+        ## Randomly select an input vector
+        
+        rand_ind <- sample(x = 1:dim(input_data)[1], size = 1)
+        input <- matrix(input_data[rand_ind, ], ncol = 1)
+        
+        ##### START HERE #####
+        
+    }
+    
+}
+
 
 
 ##### Load the Data #####
@@ -58,9 +92,14 @@ data_final <- data %>%
 data_final[data_final == "N/A"] <- NA
 data_final <- data_final[complete.cases(data_final), ]
 
+## Separate the input space from the labels
+
+output_space <- matrix(as.numeric(unlist(data_final$StyleID)), nrow = length(data_final$StyleID))
+input_space <- matrix(as.numeric(unlist(data_final[, 2:11])), nrow = nrow(data_final[, 2:11]))
+
 ## Define some data details
 
-input_size <- dim(data_final)[2] - 1
+input_size <- dim(input_space)[2]
 matrix_dim <- 15
 
 
@@ -68,6 +107,14 @@ matrix_dim <- 15
 ##### Build the Network #####
 
 
+
+## Set some network parameters
+
+ler_rate <- 0.001
+num_iter <- 5000
+radius <- matrix_dim / 2
+
+## Build the weight matrix
 
 SOM_lattice <- build_SOM(input_size, matrix_dim)
 
